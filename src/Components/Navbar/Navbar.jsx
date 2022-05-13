73275810a2b3b12ecaaf7fe9d0dcfb813a4d659d
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-05-13 08:46:06 +0100

    added content to navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,44 @@
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 function Navbar() {
 
     return (
-    <div>
-        This is the navbar
+    <div className="text-red-500">
+        <div className="container-xxl position-relative">
+            <nav className=" navbar navbar-expand-lg 
+            px-4 px-lg-5 py-3 py-lg-0 sticky-top bg-white
+            shadow-md text-red-dark p-6">
+                <Link to='/' className="navbar-brand p-3 text-red-700">
+                    <h2>
+                        Dashboard
+                    </h2>
+                </Link>
+
+                <button className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarCollapse">
+                    <FontAwesomeIcon icon={ faBars } />
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarCollapse">
+                    <div className="navbar-nav ms-auto py-0 px-3 text-start">
+                        <NavLink to='/login'
+                        className="nav-item nav-link">
+                            Sign in
+                        </NavLink>
+                        <NavLink to='/register'
+                        className="nav-item nav-link">
+                            Sign up
+                        </NavLink>
+                    </div>
+                </div>
+
+            </nav>
+        </div>
     </div>
 
 )
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div className="px-3 py-4">
+      <h2>
+        Welcome to Sometin Sometin
+      </h2>
+    </div>
   )
 }
 

9ec445587635a595b6d5ad2ae9993cb1008e78a4
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-05-18 16:10:30 +0100

    feats: revamp sidebar and navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,24 @@
 import { faBars, faSearch } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
+import Icon from '../../Assets/icon.png'
 
 function Navbar() {
 
+
+    const [isOpen, setIsOpen] = useState(true)
+
     return (
-    <div className="text-red-500 pt-2 w-full">
-        <div className="container-xxl position-relative">
+    <div className="text-red-500 pt-2 w-full -z-0">
+        <div className={` container-xxl position-relative`}>
             <nav className=" navbar navbar-expand-lg 
             px-4 px-lg-5 py-3 py-lg-0 sticky-top bg-white
             shadow-md text-red-dark p-6 ">
+                <img src= { Icon } alt="brand-logo"
+                className={`absolute cursor-pointer top-9
+                w-7 text-warning text-2xl ${!isOpen && 'rotate-180'}`}
+                onClick={() => setIsOpen(!isOpen)} />
                 <Link to='/' className="navbar-brand p-3 text-blue-800">
                     <h2>
                         Dashboard
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -3,9 +3,10 @@ import { useEffect, useState } from 'react'
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Logo from '../../Assets/sometinlogo250-150-red.png'
 
-import Navbar from '../Navbar/Navbar'
-import Icon from '../../Assets/icon.png'
-import { useLocation } from 'react-router-dom'
+
+import { Link, NavLink, useLocation } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faBarsStaggered } from '@fortawesome/free-solid-svg-icons'
 
 function Sidebar({ children}) {
 
@@ -42,10 +43,10 @@ function Sidebar({ children}) {
         <div className={`${isOpen ? 'w-52' : 'w-20'} relative duration-300
         p-4 pt-5 bg-white shadow-md login-background `}>
             <div>
-                <img src= { Icon } alt="brand-logo"
+                {/* <img src= { Icon } alt="brand-logo"
                 className={`absolute cursor-pointer -right-3 top-9
                 w-7 text-warning text-2xl ${!isOpen && 'rotate-180'}`}
-                onClick={() => setIsOpen(!isOpen)} />
+                onClick={() => setIsOpen(!isOpen)} /> */}
             </div>
             <div className="flex text-center gap-x-2 align-center items-center">
                 <img src={ Logo } alt="brand-logo"
@@ -60,8 +61,44 @@ function Sidebar({ children}) {
                 </h1>
             </div>
         </div>
-        <div>
-            <Navbar />
+
+        {/* navbar session */}
+        <div className="relative text-indigo-800">
+            <div className="containe-xxl w-full">
+                <nav className="navbar navbar-expand-lg px-4 px-lg-5
+                py-3 py-lg-0 stick-top bg-white shadow-sm text-dark">
+
+                    <FontAwesomeIcon icon={ faBarsStaggered } 
+                        className={` cursor-pointer text-[#5B0E2A] rounded-sm text-3xl font-bold
+                        ${ !isOpen && 'rotate-180'}`}
+                            onClick={() =>setIsOpen(!isOpen) }/>
+
+                    <Link to="" className="navbar-brand p-3 ">
+                        <h2>
+                            Dashboard
+                        </h2>
+                    </Link>
+
+                    <button className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarCollapse">
+                        <FontAwesomeIcon icon={ faBars } />
+                    </button>
+
+                    <div className="collapse navbar-collapse"
+                    id='#navbarCollapse'>
+                        <div className="navbar-nav ms-auto py-0 px-3 text-start">
+                            <NavLink to="/login"  className="nav-item nav-link">
+                                Login
+                            </NavLink>
+                            <NavLink to="/register" className="nav-item nav-link">
+                                Sign Up
+                            </NavLink>
+                        </div>
+                    </div>
+                </nav>
+            </div>
             { children }
         </div>
     </div>

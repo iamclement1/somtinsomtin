5f81f37089f54f4073754c22af4d1a10f26769db
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-06-21 13:40:15 +0100

    feats: working on sending otp page

--- src/Components/ForgotPassword/PhonePage.jsx
+++ src/Components/ForgotPassword/PhonePage.jsx
@@ -1,97 +1,111 @@
-import { faSpinner } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import axios from "axios";
-import { useState } from "react";
-import PhoneInput from "react-phone-number-input";
-import { useNavigate } from 'react-router-dom'
-import { toast } from "react-toastify";
 import Logo from '../../Assets/sometinlogo250-150-red.png'
+import 'react-phone-number-input/style.css'
+import PhoneInput from 'react-phone-number-input'
+import { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default function PhonePage() {
+export default function PhonePage () {
 
+    // const [value, setValue] = useState();
+    
     const navigate = useNavigate('')
 
-    const [isLoading, setIsLoading] = useState(false);
-    const [number, setNumber] = useState();
-    const auth_token = localStorage.getItem('token');
+    const [ number, setNumber ] = useState('');
+    const [ isLoading, setIsLoading ] = useState(false);
 
+    useEffect(() => {
+        localStorage.setItem('number', JSON.stringify(number));
+        setNumber(number);
+        
+    }, [ number])
 
     const handleSubmit = (event) => {
         setIsLoading(true)
         event.preventDefault();
-
         const data = {
-            "phone_number": number
+            "phone_number": number,
         }
+        // console.log('data', data);        
+        axios.post('https://somtinsomtin-api.herokuapp.com/api/v1.0/users/check_phone_number/', data)
+        .then( response => {
+            console.log(response.data);
+            toast.success('OTP successfully sent!')
+            setIsLoading(false);
 
-        axios.post("https://somtinsomtin-api.herokuapp.com/api/v1.0/users/check_phone_number/", data, {
-            headers: {
-                Authorization: `Bearer ${auth_token}`
+            if (response.data.response_code === "200") {
+                navigate('/forget-password')
+            } else {
+                toast.error("User Already Exist")
             }
+            
         })
-            .then(response => {
-                localStorage.setItem('number')
-                console.log(response.data)
-                toast('otp sent successfully')
-                setIsLoading(false)
 
-                if (response.data.response_code === "100") {
-                    navigate('/forgot-password')
-                }
-            })
-            .catch(error => {
-                console.log(error.response)
-                toast.error('number not found')
-                setIsLoading(false)
-            })
+        .catch( error => {
+            console.log(error.response)
+            toast.error('Phone number invalid')
+            setIsLoading(false)
+        })
     }
 
     return (
-        <div className=" container-xxl position-relative login-background h-screen flex bg-gray-bg1
-        font-sans">
-            <div className="w-full max-w-md m-auto bg-white rounded-lg 
-        shadow-md py-8 md:px-16 px-8">
-                <img src={Logo} alt="brand-logo"
-                    className="w-14 m-auto" />
-                <h1 className='text-sm font-medium text-dark mt-2 mb-2 text-center'>
-                    Enter Phone Number for Verfication
-                </h1>
+    <div className=" container-xxl position-relative login-background h-screen
+    flex bg-gray-bg1 font-sans">
+        <div className="w-full max-w-md m-auto bg-white rounded-lg 
+        shadow-md py-8 md:px-16 px-6">
+            <img src={ Logo } alt="brand-logo"
+            className="w-14 m-auto" />
+            <h1 className='text-sm font-medium text-dark mt-2 mb-2 text-center'> 
+                Enter your phone number for verfication
+            </h1>
 
-                <form action="">
-                    <div>
-                        <label htmlFor="Phone Number"
-                            className="font-semibold text-gray-400"></label>
-                        <PhoneInput placeholder="Enter phone number"
-                            international
-                            defaultCountry='GH'
-                            value={number}
-                            onChange={setNumber}
-                            className={`w-full p-2 text-gray-400 border rounded-md outline-none 
-                            text-sm transition duration-150 ease-in-out mb-4 focus:outline-none`} />
-                    </div>
+            <form action="">
+                <div>
+                    <label htmlFor="Phone Number"
+                    className='font-semibold text-gray-400 font-sans'>
+                        
+                    </label>
+                    <PhoneInput placeholder="Enter phone number"
+                    international
+                    defaultCountry='GH'
+                    value={number}
+                    onChange={ setNumber }
+                    id='phone_number'
+                    className= {`md:w-full p-2 text-gray-400 border rounded-md outline-none 
+                    text-sm transition duration-150 ease-in-out mb-4 focus:outline-none
+                    `} />
+                </div>
 
-                    <div className=''>
-                        {!isLoading && (
-                            <button className="bg-red-400
-                    font-semibold text-white mr-2
-                    hover:bg-red-700 hover:text-white p-2 w-full mb-2 rounded-sm"
-                                onClick={handleSubmit}>
-                                Verify Number
-                            </button>
-
-                        )}
-                        {isLoading && (
-                            <button className="bg-red-400 text-sm text-white space-x-5
-                        hover:bg-gradient hover:text-white p-2 w-full mb-2 rounded-sm"
-                                disabled>
-                                <FontAwesomeIcon icon={faSpinner}
-                                    className='space-x-4 spinner mr-3' />
-                                loading...
-                            </button>
-                        )}
-                    </div>
-                </form>
-            </div>
+            
+                <div>
+                    { !isLoading && (
+                    <button className="bg-red-400
+                    font-semibold text-white rounded-sm 
+                    hover:bg-red-700
+                    hover:text-white p-2 w-full mb-2"
+                    onClick={ handleSubmit }>
+                        Get Started
+                    </button>
+                    )}
+                    { isLoading && (
+                        <button className="bg-red-400
+                        text-sm text-white rounded-sm 
+                        hover:bg-red-600
+                        hover:text-white p-2 w-full mb-2"
+                        disabled>
+                            <FontAwesomeIcon icon={ faSpinner } 
+                            className='space-x-4 spinner mr-3'/>
+                            sending otp...
+                        </button>
+                    )}
+                </div>
+            </form>
         </div>
+    </div>
+
     )
 }

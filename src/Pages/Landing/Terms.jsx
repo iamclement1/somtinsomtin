1ed2ada738ebe87143beb99b8e4b75b8a55d393f
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-06-27 13:41:42 +0100

    feats: added back to top button

--- src/Pages/Landing/Terms.jsx
+++ src/Pages/Landing/Terms.jsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from "react";
+import { BsFillArrowUpSquareFill } from "react-icons/bs";
 import Navbar from "../../Components/Navbar/Navbar";
 
 export default function Terms() {
+    const [ showButton, setShowButton ] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.pageYOffset > 250 ) {
+                setShowButton(true);
+            } else {
+                setShowButton(false);
+            }
+        })
+    }, [])
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    }
+    
     return (
         <main className="">
             <Navbar />
@@ -146,6 +167,11 @@ export default function Terms() {
                         </div>
                     </div>
                 </div>
+                { showButton && (
+                <button onClick={scrollToTop} className="back-to-top">
+                    <BsFillArrowUpSquareFill />
+                </button>
+            )}
             </div>
         </main>
     )

9bf8595c710ed2c247af4349b4906ac78d4a3316
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-07-01 10:30:29 +0100

    feats: added loader in project

--- src/App.js
+++ src/App.js
@@ -4,18 +4,30 @@ import { ToastContainer } from 'react-toastify'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css'
 import Routing from './Components/router/router';
+import { useEffect, useState } from 'react';
+import Loader from './Components/Loader/Loader';
 
 
 function App( className="font-roboto") {
   
+  const [ loading, setLoading ] = useState(false)
   
+  useEffect(() => {
+    setLoading(true);
+  }, [])
+
   return (
     <Router>
+      { !loading ? (
+        <>
+        <Loader />
+        </>
+      ) : (
       <div className="App">
       <ToastContainer />
         <Routing />
       </div>
-      
+      )}
     </Router>
   );
 }
